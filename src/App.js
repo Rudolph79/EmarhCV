ac946f019b5f2144b2c7276f2c17e9eb24a869ea
Rudolph79 <rudolph.ebang@gmail.com>
2023-03-25 17:19:16 +0100

    Login, Register and Home pages created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
+import 'antd/dist/reset.css';
+import { DatePicker } from 'antd';
+import { Button } from 'antd';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import Home from './pages/Home';
 
 const App = () => {
   return (
     <div className="App">
-      <h1>Emarh CV</h1>
+      <Router>
+        <Routes>
+          <Route path="/connexion" element={<Login />} />
+          <Route path="/inscription" element={<Register />} />
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
